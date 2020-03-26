9eb62da40ee5af9f6e485b21df6bb88c492cfa78
ayumitk <ayumee528@gmail.com>
2020-03-26 11:16:04 -0700

    update plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,8 +47,12 @@ module.exports = {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 1200,
+              quality: 90,
+              withWebp: true,
+              linkImagesToOriginal: false,
             },
           },
+          `gatsby-remark-lazy-load`,
           {
             resolve: 'gatsby-remark-responsive-iframe',
             options: {
@@ -97,17 +101,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Ayumi Takahashi - Freelance UI/UX Designer`,
+        short_name: `ayumi.tk`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FF5851`,
+        theme_color: `#FF5851`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
