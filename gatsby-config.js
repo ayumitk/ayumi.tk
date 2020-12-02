a4400f0098604537f18505aca49735e519262661
ayumitk <ayumee528@gmail.com>
2020-12-02 10:04:08 -0800

    add: google analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -124,6 +124,15 @@ module.exports = {
       },
     },
     `gatsby-plugin-sitemap`,
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-121956784-1',
+        head: true,
+        respectDNT: true,
+        pageTransitionDelay: 250,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
